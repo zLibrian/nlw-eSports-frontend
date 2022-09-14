2ed5998d3a362413491b78b413f8993f5e7d470e
Kevin Willian <kevinwillian084@gmail.com>
2022-09-14 02:00:40 -0300

    feat: adiciona gradientes do texto principal e das boxs de jogos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
     "./index.html"
   ],
   theme: {
+    extend: {
+      backgroundImage: {
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 10.08%, #43E7AD 38.94%, #E1D55D 85.57%)',
+        'game-text-box-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)', 
         galaxy: "url('./src/assets/background-galaxy.png')"
+      },
+    },
   },
   plugins: [],
 }
