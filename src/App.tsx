e8cdc510c15c72acd59da197c5d3523395a75d51
Kevin Willian <kevinwillian084@gmail.com>
2022-09-14 02:02:38 -0300

    feat: adiciona logo e o texto da chamada com estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import logoImg from './assets/nlw-logo.svg'
 import "./styles/main.css"
 
 function App() {
   return (
-    <div>
-      Configuracoes iniciais
+    <div className='max-w-[1334px] mx-auto flex flex-col items-center my-20 justify-between'>
+      <img src={logoImg} width="400px" alt="nwl logo" />
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='bg-clip-text bg-nlw-gradient text-transparent'>
+          duo
+        </span> está aqui.</h1>
     </div>
-  )
+  ) 
 }
 
 export default App
